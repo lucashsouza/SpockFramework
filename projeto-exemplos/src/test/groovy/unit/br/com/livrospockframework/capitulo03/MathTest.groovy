4aad0e939376281a46e154399835c13855dc4c71
Lucas Souza <lhenriquesouza00@gmail.com>
2021-01-01 11:43:13 -0300

    5.2 UM MÉTODO, VÁRIOS CENÁRIOS

--- projeto-exemplos/src/test/groovy/unit/br/com/livrospockframework/capitulo03/MathTest.groovy
+++ projeto-exemplos/src/test/groovy/unit/br/com/livrospockframework/capitulo03/MathTest.groovy
@@ -1,16 +1,38 @@
 package unit.br.com.livrospockframework.capitulo03
+
 import spock.lang.Specification
 
 class MathTest extends Specification {
-    def '2 ao quadrado deve ser 4'(){
+    def '2 ao quadrado deve ser 4'() {
         expect:
         Math.pow(2, 2) == 4
     }
 
-    def 'sqrt deve calcular a raiz quadrada'(){
+    def 'sqrt deve calcular a raiz quadrada'() {
         expect:
         Math.sqrt(4) == 2
         Math.sqrt(25) == 5
         Math.sqrt(144) == 12
     }
+
+/*  Teste que falha por exceção
+
+    def '2 ao quadrado deve ser 4'(){
+        expect:
+        Math.pow((1/0), 2) == 4             // java.lang.ArithmeticException: Division by zero
+    }
+
+    def '2 ao quadrado deve ser 4'(){
+        Double base = null
+        expect:
+        Math.pow(base.intValue(), 2) == 4   // java.lang.NullPointerException:
+    }
+
+    Teste que não passa em uma verificação
+
+    def '2 ao quadrado deve ser 4'() {
+        expect:
+        Math.pow(2, 2) == 5
+    }
+*/
 }
---
+++ projeto-exemplos/src/test/groovy/unit/br/com/livrospockframework/capitulo05/FaixaImcTest.groovy
@@ -0,0 +1,47 @@
+package unit.br.com.livrospockframework.capitulo05
+
+import br.com.livrospockframework.capitulo05.enums.FaixaImc
+import br.com.livrospockframework.capitulo05.enums.Sexo
+import spock.lang.Specification
+
+class FaixaImcTest extends Specification {
+
+    def 'IMC deve estar na faixa correta'() {
+        setup:
+        def cenarios = [
+                [imc: 18, sexo: Sexo.FEMININO, condicao: FaixaImc.ABAIXO],
+                [imc: 21, sexo: Sexo.FEMININO, condicao: FaixaImc.NORMAL],
+                [imc: 27, sexo: Sexo.FEMININO, condicao: FaixaImc.ACIMA],
+                [imc: 20, sexo: Sexo.MASCULINO, condicao: FaixaImc.ABAIXO],
+                [imc: 23, sexo: Sexo.MASCULINO, condicao: FaixaImc.NORMAL],
+                [imc: 28, sexo: Sexo.MASCULINO, condicao: FaixaImc.ACIMA],
+        ]
+
+        expect:
+        cenarios.each {
+            assert  FaixaImc.getFaixa(it.imc, it.sexo) == it.condicao
+        }
+    }
+
+    def 'IMC deve estar na faixa correta com Data-Driven Testing'(){
+        expect:
+        FaixaImc.getFaixa(imc, sexo) == resultado
+
+        where:
+        imc     |   sexo            | resultado
+        18      |   Sexo.FEMININO   | FaixaImc.ABAIXO
+        21      |   Sexo.FEMININO   | FaixaImc.NORMAL
+        27      |   Sexo.FEMININO   | FaixaImc.ACIMA
+        20      |   Sexo.MASCULINO  | FaixaImc.ABAIXO
+        23      |   Sexo.MASCULINO  | FaixaImc.NORMAL
+        28      |   Sexo.MASCULINO  | FaixaImc.ACIMA
+        18.99   |   Sexo.FEMININO   | FaixaImc.ABAIXO
+        19      |   Sexo.FEMININO   | FaixaImc.NORMAL
+        25.99   |   Sexo.FEMININO   | FaixaImc.NORMAL
+        26      |   Sexo.FEMININO   | FaixaImc.ACIMA
+        20.99   |   Sexo.MASCULINO   | FaixaImc.ABAIXO
+        21      |   Sexo.MASCULINO   | FaixaImc.NORMAL
+        26.99   |   Sexo.MASCULINO   | FaixaImc.NORMAL
+        27      |   Sexo.MASCULINO   | FaixaImc.ACIMA
+    }
+}
